653768ca6fa738f4a72704cc0354967c470b2e15
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-08 11:57:47 -0800

    skeleton

---
+++ setup.py
@@ -0,0 +1,66 @@
+"""Packaging settings."""
+
+
+from codecs import open
+from os.path import abspath, dirname, join
+from subprocess import call
+
+from setuptools import Command, find_packages, setup
+
+from idseq_pipeline import __version__
+
+
+this_dir = abspath(dirname(__file__))
+with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
+    long_description = file.read()
+
+
+class RunTests(Command):
+    """Run all tests."""
+    description = 'run tests'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Run all tests!"""
+        errno = call(['py.test', '--cov=idseq_pipeline', '--cov-report=term-missing'])
+        raise SystemExit(errno)
+
+
+setup(
+    name = 'idseq_pipeline',
+    version = __version__,
+    description = 'IDseq pipeline',
+    long_description = long_description,
+    url = 'https://github.com/chanzuckerberg/idseq-pipeline',
+    author = 'idseq-pipeline contributors',
+    author_email = 'cdebourcy@chanzuckerberg.com',
+    license = 'UNLICENSE',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords = 'cli',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires = ['docopt'],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'idseq_pipeline=idseq_pipeline.cli:main',
+        ],
+    },
+    cmdclass = {'test': RunTests},
+)
