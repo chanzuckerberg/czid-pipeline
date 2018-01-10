2e62f51398486935da8ee69ca874c0fee8cffd78
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-10 10:22:18 -0800

    add commit sha upload

--- idseq_pipeline/commands/host_filtering.py
+++ idseq_pipeline/commands/host_filtering.py
@@ -7,9 +7,7 @@ class Host_filtering(Base):
     def run(self):
         from .host_filtering_functions import *
 
-        # Unbuffer stdout and redirect stderr into stdout.  This helps observe logged events in realtime.
-        sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-        os.dup2(sys.stdout.fileno(), sys.stderr.fileno())
+        unbuffer_stdout()
+        upload_commit_sha()
 
-        # execute the pipeline stage
         run_stage1(True)
--- idseq_pipeline/commands/host_indexing.py
+++ idseq_pipeline/commands/host_indexing.py
@@ -7,9 +7,7 @@ class Host_indexing(Base):
     def run(self):
         from .host_indexing_functions import *
 
-        # Unbuffer stdout and redirect stderr into stdout.  This helps observe logged events in realtime.
-        sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-        os.dup2(sys.stdout.fileno(), sys.stderr.fileno())
+        unbuffer_stdout()
+        upload_commit_sha()
 
-        # make indexes
         make_indexes(True)
