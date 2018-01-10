2a6294ac273a564b70cb6b069975f520ab50a980
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-10 15:26:12 -0800

    gunzip if necessary

--- idseq_pipeline/commands/host_indexing_functions.py
+++ idseq_pipeline/commands/host_indexing_functions.py
@@ -124,7 +124,7 @@ def make_indexes(lazy_run = True):
         version_number = get_reference_version_number(ncbitool_path, INPUT_FASTA_S3)
         input_fasta_local = download_reference_locally(ncbitool_path, INPUT_FASTA_S3, version_number, fasta_dir)
 
-    if os.path.splitext(input_fasta_local)[1] == ".gz"
+    if os.path.splitext(input_fasta_local)[1] == ".gz":
         execute_command("gunzip -f %s" % input_fasta_local)
         input_fasta_local = os.path.splitext(input_fasta_local)[0]
 
