0d02badc4f11c6182dec9796b67609629539774f
cdebourcy <cdebourcy@chanzuckerberg.com>
2018-01-10 13:03:50 -0800

    switch to ncbitool for retrieving reference files & versions (#11)
    * switch to ncbitool
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix bug

--- idseq_pipeline/commands/gsnap_indexing_functions.py
+++ idseq_pipeline/commands/gsnap_indexing_functions.py
@@ -9,7 +9,7 @@ KEY_S3_PATH = os.environ.get('KEY_S3_PATH')
 OUTPUT_PATH_S3 = os.environ.get('OUTPUT_PATH_S3')
 OUTPUT_NAME = os.environ.get('OUTPUT_NAME')
 
-# gzipped FASTA reference to index
+# path to gzipped FASTA reference to index, relative to ftp://ftp.ncbi.nlm.nih.gov
 INPUT_FASTA_S3 = os.environ.get('INPUT_FASTA_S3')
 
 # location of gmapdb, specified during compilation (see https://github.com/juliangehring/GMAP-GSNAP/blob/master/README)
@@ -18,24 +18,60 @@ GMAPDB_PATH = "/home/ubuntu/share"
 # location of gmap-gsnap executables
 GSNAPL_PATH = "/home/ubuntu/bin"
 
-# working directory on machine to use
-WORK_DIR = "/home/ubuntu/gmap_build_workdir"
+# S3 location of ncbitool executable
+NCBITOOL_S3_PATH = "s3://czbiohub-infectious-disease/ncbitool"
+
+# working directories
+WORK_DIR = "/home/ubuntu/gmap_build_workdir" # on GSNAP machine
+REMOTE_USERNAME = "ubuntu"
+KEY_PATH = None
+LOCAL_WORK_DIR = "idseq_pipeline_temp" # locally
+
+def get_key():
+    global KEY_PATH
+    execute_command("aws s3 cp %s %s/" % (KEY_S3_PATH, LOCAL_WORK_DIR))
+    KEY_PATH = os.path.join(LOCAL_WORK_DIR, os.path.basename(KEY_S3_PATH))
+    execute_command("chmod 400 %s" % KEY_PATH)
+
+def install_ncbitool():
+    # install locally
+    execute_command("aws s3 cp %s %s/" % (NCBITOOL_S3_PATH, LOCAL_WORK_DIR))
+    execute_command("chmod u+x %s/ncbitool" % LOCAL_WORK_DIR)
+    # install on remote
+    command = "aws s3 cp %s %s/; " % (NCBITOOL_S3_PATH, WORK_DIR)
+    command += "chmod u+x %s/ncbitool" % WORK_DIR
+    execute_command(remote_command(command, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
+
+def get_reference_version_number():
+    command = "%s/ncbitool file history %s" % (LOCAL_WORK_DIR, INPUT_FASTA_S3)
+    output = execute_command_with_output(command).split("File History: ")[1]
+    version_history = json.loads(output)
+    version_numbers = [entry["Version"] for entry in version_history]
+    return max(version_numbers) # use latest available version
+
+def download_reference_on_remote(version_number):
+    command = "cd %s; ./ncbitool file --download --version-num %s %s" % (WORK_DIR, version_number, INPUT_FASTA_S3)
+    execute_command(remote_command(command, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
+    return os.path.join(WORK_DIR, os.path.basename(INPUT_FASTA_S3))
 
 def make_index():
-    # get key
-    execute_command("aws s3 cp %s %s/" % (KEY_S3_PATH, WORK_DIR))
-    key_path = os.path.join(WORK_DIR, os.path.basename(KEY_S3_PATH))
-    execute_command("chmod 400 %s" % key_path)
-    remote_username = "ubuntu"
-    # get input fasta
-    download_command = "aws s3 cp %s %s/" % (INPUT_FASTA_S3, WORK_DIR)
-    input_fasta_zipped = os.path.join(WORK_DIR, os.path.basename(INPUT_FASTA_S3))
-    input_fasta_unzipped = os.path.join(WORK_DIR, os.path.basename(INPUT_FASTA_S3)[:-3])
-    download_command += "; rm %s; gunzip %s" % (input_fasta_unzipped, input_fasta_zipped)
-    execute_command(remote_command(download_command, key_path, remote_username, SERVER_IP))
+    # Set up
+    execute_command("mkdir -p %s" % LOCAL_WORK_DIR)
+    get_key()
+    execute_command(remote_command("mkdir -p %s" % WORK_DIR, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
+    install_ncbitool()
+
+    # Get latest version of reference and unzip
+    version_number = get_reference_version_number()
+    input_fasta_zipped = download_reference_on_remote(version_number)
+    input_fasta_unzipped = input_fasta_zipped[:-3]
+    command = "gunzip -f %s" % input_fasta_zipped
+    execute_command(remote_command(command, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
+
     # make index
     indexing_command = "sudo %s/gmap_build -d %s -k 16 %s" % (GSNAPL_PATH, OUTPUT_NAME, input_fasta_unzipped)
-    execute_command(remote_command(indexing_command, key_path, remote_username, SERVER_IP))
+    execute_command(remote_command(indexing_command, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
+
     # upload index
     upload_command = "aws s3 cp %s/%s %s/%s/ --recursive" % (GMAPDB_PATH, OUTPUT_NAME, OUTPUT_PATH_S3, OUTPUT_NAME)
-    execute_command(remote_command(upload_command, key_path, remote_username, SERVER_IP))
+    execute_command(remote_command(upload_command, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
