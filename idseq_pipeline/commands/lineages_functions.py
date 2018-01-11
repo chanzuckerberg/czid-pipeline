84bfe0c46cd5b91c80840656df44d88e14054d79
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-11 13:56:17 -0800

    add deuterostome list

--- idseq_pipeline/commands/lineages_functions.py
+++ idseq_pipeline/commands/lineages_functions.py
@@ -13,16 +13,24 @@ def make_lineages():
     execute_command("cd ncbitax2lin")
     work_dir = os.getcwd()
 
-    # Get input reference and version number.
+    # Get input reference and version number
     ncbitool_path = install_ncbitool(work_dir)
     version_number = get_reference_version_number(ncbitool_path, INPUT)
     input_fasta_local = download_reference_locally(ncbitool_path, INPUT, version_number, work_dir)
-    execute_command("cd %s; rm -rfv taxdump; mkdir -p taxdump/taxdump; tar zxf %s -C ./taxdump/taxdump" % (work_dir, os.path.basename(INPUT)))
+    execute_command("cd %s; rm -rfv taxdump; mkdir -p taxdump/taxdump" % work_dir)
+    execute_command("tar zxf %s -C ./taxdump/taxdump" % os.path.basename(INPUT)) # this structure is needed for ncbitax2lin 'make'
 
-    # Make lineage files
+    # Make and upload lineage files
     execute_command("cd %s; make" % work_dir)
     execute_command("aws s3 cp taxid-lineages.db %s/" % OUTPUT_PATH_S3)
     execute_command("aws s3 cp taxid-lineages.csv.gz %s/" % OUTPUT_PATH_S3)
     execute_command("aws s3 cp names.csv.gz %s/" % OUTPUT_PATH_S3)
 
-    # Make deuterostome list
+    # Make and upload deuterostome list
+    input_filename = "lineages.csv"
+    output_filename = "deuterostome_taxids.txt"
+    execute_command("cd %s; gunzip %s.gz; grep 'Deuterostomia' %s | cut -f1 -d',' > %s" % (work_dir, input_filename, input_filename, output_filename))
+    execute_command("aws s3 cp %s %s/" % (output_filename, OUTPUT_PATH_S3))
+
+    # upload version:
+    upload_version_tracker('lineage_and_deuterostome', version_number, OUTPUT_PATH_S3)
