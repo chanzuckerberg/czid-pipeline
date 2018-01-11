3c88eeb79d91711117bdcd962db5288005066ed7
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-10 17:24:38 -0800

    add rapsearch indexing

--- idseq_pipeline/cli.py
+++ idseq_pipeline/cli.py
@@ -31,7 +31,7 @@ Examples:
 
   INPUT_FASTA_S3=/blast/db/FASTA/nt.gz SERVER_IP=34.211.67.166 KEY_S3_PATH=s3://czbiohub-infectious-disease/idseq-production.pem OUTPUT_PATH_S3=s3://czbiohub-infectious-disease/references OUTPUT_NAME=nt_k16 idseq_pipeline gsnap_indexing
 
-  INPUT_FASTA_S3=/blast/db/FASTA/nr.gz SERVER_IP=54.191.193.210 KEY_S3_PATH=s3://czbiohub-infectious-disease/idseq-production.pem OUTPUT_PATH_S3=s3://czbiohub-infectious-disease/references OUTPUT_NAME=nr_rapsearch idseq_pipeline rapsearch_indexing
+  INPUT_FASTA_S3=/blast/db/FASTA/nr.gz SERVER_IP=54.191.193.210 KEY_S3_PATH=s3://czbiohub-infectious-disease/idseq-alpha.pem OUTPUT_PATH_S3=s3://czbiohub-infectious-disease/references OUTPUT_NAME=nr_rapsearch idseq_pipeline rapsearch_indexing
 
   INPUT_FASTA_S3='s3://czbiohub-ncbi-store/blast/db/FASTA/vector.gz' ACCESSION2TAXID_DB_S3_PATH='s3://czbiohub-infectious-disease/references/accession2taxid.db.gz' idseq_pipeline blacklist
 
---
+++ idseq_pipeline/commands/rapsearch_indexing_functions.py
@@ -0,0 +1,61 @@
+import os
+from .common import *
+
+# address and key of RAPSearch machine to use
+SERVER_IP = os.environ.get('SERVER_IP')
+KEY_S3_PATH = os.environ.get('KEY_S3_PATH')
+
+# output location and name
+OUTPUT_PATH_S3 = os.environ.get('OUTPUT_PATH_S3')
+OUTPUT_NAME = os.environ.get('OUTPUT_NAME')
+
+# path to gzipped FASTA reference to index, relative to ftp://ftp.ncbi.nlm.nih.gov
+INPUT_FASTA_S3 = os.environ.get('INPUT_FASTA_S3')
+
+# location of index builder executable
+PRE_RAPSEARCH_PATH = "/usr/local/bin/prerapsearch"
+
+# S3 location of ncbitool executable
+NCBITOOL_S3_PATH = "s3://czbiohub-infectious-disease/ncbitool"
+
+# working directories
+WORK_DIR = "/home/ec2-user/data/pre_rapsearch_workdir" # on RAPSearch machine
+REMOTE_USERNAME = "ec2-user"
+KEY_PATH = None
+LOCAL_WORK_DIR = "idseq_pipeline_temp" # locally
+
+def get_key():
+    global KEY_PATH
+    execute_command("aws s3 cp %s %s/" % (KEY_S3_PATH, LOCAL_WORK_DIR))
+    KEY_PATH = os.path.join(LOCAL_WORK_DIR, os.path.basename(KEY_S3_PATH))
+    execute_command("chmod 400 %s" % KEY_PATH)
+
+def make_index():
+    # Set up
+    execute_command("mkdir -p %s" % LOCAL_WORK_DIR)
+    get_key()
+    execute_command(remote_command("sudo mkdir -p %s" % WORK_DIR, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
+    local_ncbitool, remote_ncbitool = install_ncbitool(LOCAL_WORK_DIR, WORK_DIR, KEY_PATH, REMOTE_USERNAME, SERVER_IP)
+
+    # get latest version number of desired reference file
+    version_number = get_reference_version_number(local_ncbitool, INPUT_FASTA_S3)
+
+    # download reference and unzip
+    input_fasta_zipped = download_reference_on_remote(remote_ncbitool, INPUT_FASTA_S3, version_number, WORK_DIR,
+                                                      KEY_PATH, REMOTE_USERNAME, SERVER_IP)
+    input_fasta_unzipped = input_fasta_zipped[:-3]
+    command = "sudo gunzip -f %s" % input_fasta_zipped
+    execute_command(remote_command(command, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
+
+    # make index
+    remote_output_dir = os.path.join(WORK_DIR, OUTPUT_NAME)
+    execute_command(remote_command("sudo mkdir -p %s" % remote_output_dir, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
+    indexing_command = "cd %s; %s -d %s -n %s" % (remote_output_dir, PRE_RAPSEARCH_PATH, input_fasta_unzipped, OUTPUT_NAME)
+    execute_command(remote_command(indexing_command, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
+
+    # upload index
+    upload_command = "aws s3 cp %s %s/%s/ --recursive" % (remote_output_dir, OUTPUT_PATH_S3, OUTPUT_NAME)
+    execute_command(remote_command(upload_command, KEY_PATH, REMOTE_USERNAME, SERVER_IP))
+
+    # upload version tracker file
+    upload_version_tracker(OUTPUT_NAME, version_number, OUTPUT_PATH_S3)
