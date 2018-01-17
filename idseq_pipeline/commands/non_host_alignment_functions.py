192f34f8e7e04f65b49c7a8f8dc089e3b787c73e
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-17 15:37:59 -0800

    remove subsampled path suffic

--- idseq_pipeline/commands/non_host_alignment_functions.py
+++ idseq_pipeline/commands/non_host_alignment_functions.py
@@ -52,8 +52,7 @@ STATS_OUT = 'stats.json'
 VERSION_OUT = 'versions.json' 
 
 # arguments from environment variables
-SUBSAMPLE = os.environ.get('SUBSAMPLE') # number of reads to subsample to, before gsnap
-subsampling_suffix = "/subsample_" + str(SUBSAMPLE) if SUBSAMPLE else ""
+SUBSAMPLE = os.environ.get('SUBSAMPLE') # number of read pairs to subsample to, before gsnap
 FASTQ_BUCKET = os.environ.get('FASTQ_BUCKET')
 INPUT_BUCKET = os.environ.get('INPUT_BUCKET')
 FILE_TYPE = os.environ.get('FILE_TYPE', 'fastq.gz')
@@ -62,12 +61,12 @@ AWS_BATCH_JOB_ID = os.environ.get('AWS_BATCH_JOB_ID', 'local')
 ENVIRONMENT = os.environ.get('ENVIRONMENT', 'production')
 SAMPLE_S3_FASTQ_PATH = FASTQ_BUCKET.rstrip('/')
 SAMPLE_S3_INPUT_PATH = INPUT_BUCKET.rstrip('/')
-SAMPLE_S3_OUTPUT_PATH = OUTPUT_BUCKET.rstrip('/') + subsampling_suffix
+SAMPLE_S3_OUTPUT_PATH = OUTPUT_BUCKET.rstrip('/')
 SAMPLE_S3_OUTPUT_CHUNKS_PATH = os.path.join(SAMPLE_S3_OUTPUT_PATH, "chunks")
 SAMPLE_NAME = SAMPLE_S3_INPUT_PATH[5:].rstrip('/').replace('/','-')
 SAMPLE_DIR = DEST_DIR + '/' + SAMPLE_NAME
 FASTQ_DIR = SAMPLE_DIR + '/fastqs'
-RESULT_DIR = SAMPLE_DIR + '/results' + subsampling_suffix
+RESULT_DIR = SAMPLE_DIR + '/results'
 CHUNKS_RESULT_DIR = os.path.join(RESULT_DIR, "chunks")
 DEFAULT_LOGPARAMS = {"sample_s3_output_path": SAMPLE_S3_OUTPUT_PATH,
                      "stats_file": os.path.join(RESULT_DIR, STATS_OUT)}
