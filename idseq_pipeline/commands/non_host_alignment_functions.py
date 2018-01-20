768050f33f3a5d635f4f26833507cbb61f1fc3b5
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-01-19 17:38:50 -0800

    debug

--- idseq_pipeline/commands/non_host_alignment_functions.py
+++ idseq_pipeline/commands/non_host_alignment_functions.py
@@ -914,6 +914,8 @@ def run_stage2(lazy_run = True):
         subsampled_gsnapl_input_files, subsampled_merged_fasta = subsample_fastas(gsnapl_input_files, merged_fasta, target_n_reads)
         gsnapl_input_files = subsampled_gsnapl_input_files
         merged_fasta = subsampled_merged_fasta
+        for file in gsnapl_input_files + [merged_fasta]:
+            execute_command("aws s3 cp %s %s/" % (file, SAMPLE_S3_OUTPUT_PATH))
 
     # run gsnap remotely
     logparams = return_merged_dict(DEFAULT_LOGPARAMS,
