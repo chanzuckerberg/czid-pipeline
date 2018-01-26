04987fee5bfe3c4ba8cf1cc5c05bbd8ac0ef13bf
cdebourcy <cdebourcy@chanzuckerberg.com>
2018-01-26 13:59:56 -0800

    Fix corruptness check when there are no hits (#28)
    * Update non_host_alignment_functions.py
    
    * Update non_host_alignment_functions.py
    
    * Update non_host_alignment_functions.py

--- idseq_pipeline/commands/non_host_alignment_functions.py
+++ idseq_pipeline/commands/non_host_alignment_functions.py
@@ -630,6 +630,15 @@ def clean_direct_gsnapl_input(fastq_files):
         execute_command("aws s3 cp %s %s/" % (f, SAMPLE_S3_OUTPUT_PATH))
     return cleaned_files, file_type_for_log
 
+def interpret_min_column_number_string(min_column_number_string, correct_number_of_output_columns):
+    if min_column_number_string:
+        min_column_number = float(min_column_number_string)
+        write_to_log("Try no. %d: Smallest number of columns observed in any line was %d" % (try_number, min_column_number))
+    else:
+        write_to_log("Try no. %d: No hits" % try_number)
+        min_column_number = correct_number_of_output_columns
+    return min_column_number
+
 def run_gsnapl_chunk(part_suffix, remote_home_dir, remote_index_dir, remote_work_dir, remote_username,
                      input_files, key_path, lazy_run):
         chunk_id = input_files[0].split(part_suffix)[-1]
@@ -660,8 +669,8 @@ def run_gsnapl_chunk(part_suffix, remote_home_dir, remote_index_dir, remote_work
                 execute_command(remote_command(commands, key_path, remote_username, gsnapl_instance_ip))
                 # check if every row has correct number of columns (12) in the output file on the remote machine
                 verification_command = "awk '{print NF}' %s | sort -nu | head -n 1" % remote_outfile
-                min_column_number = float(execute_command_with_output(remote_command(verification_command, key_path, remote_username, gsnapl_instance_ip)))
-                write_to_log("Try no. %d: Smallest number of columns observed in any line was %d" % (try_number, min_column_number))
+                min_column_number_string = execute_command_with_output(remote_command(verification_command, key_path, remote_username, gsnapl_instance_ip))
+                min_column_number = interpret_min_column_number_string(min_column_number_string, correct_number_of_output_columns)
                 try_number += 1
             # move output from remote machine to s3
             assert min_column_number == correct_number_of_output_columns, "Chunk %s output corrupt; not copying to S3. Re-start pipeline to try again." % chunk_id
@@ -781,8 +790,8 @@ def run_rapsearch_chunk(part_suffix, remote_home_dir, remote_index_dir, remote_w
             # check if every row has correct number of columns (12) in the output file on the remote machine
             verification_command = "grep -v '^#' %s" % output_path # first, remove header lines starting with '#'
             verification_command += " | awk '{print NF}' | sort -nu | head -n 1"
-            min_column_number = float(execute_command_with_output(remote_command(verification_command, key_path, remote_username, instance_ip)))
-            write_to_log("Try no. %d: Smallest number of columns observed in any line was %d" % (try_number, min_column_number))
+            min_column_number_string = execute_command_with_output(remote_command(verification_command, key_path, remote_username, instance_ip))
+            min_column_number = interpret_min_column_number_string(min_column_number_string, correct_number_of_output_columns)
             try_number += 1
         # move output from remote machine to s3
         assert min_column_number == correct_number_of_output_columns, "Chunk %s output corrupt; not copying to S3. Re-start pipeline to try again." % chunk_id
