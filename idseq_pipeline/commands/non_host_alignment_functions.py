91abb75aa0472e31197b411543a1c6459af45043
Yun-Fang Juan <yunfangjuan@users.noreply.github.com>
2018-02-27 19:37:30 +0000

    dont need this

--- idseq_pipeline/commands/non_host_alignment_functions.py
+++ idseq_pipeline/commands/non_host_alignment_functions.py
@@ -11,7 +11,6 @@ import traceback
 import random
 import multiprocessing
 from .common import * #pylint: disable=wildcard-import
-from .accessionid2seq import *
 
 
 # that's per job;  by default in early 2018 jobs are subsampled to <= 100 chunks
