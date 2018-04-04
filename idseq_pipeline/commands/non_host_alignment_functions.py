271537c37191bf6dcaf606a66db1dc8229402fff
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-04-03 17:50:30 -0700

    make json WIP

--- idseq_pipeline/commands/non_host_alignment_functions.py
+++ idseq_pipeline/commands/non_host_alignment_functions.py
@@ -847,7 +847,7 @@ def call_hits_m8(input_m8, output_m8, output_summary):
             hits = add_taxid_hits(acc, hits)
         hit_level, taxid = call_hit_level(read_id, hits)
         outf.write(first_line + "\n")
-        outf_sum.write("%s\t%d\t%d\n" % (read_id, hit_level, taxid))
+        outf_sum.write("%s\t%d\t%s\n" % (read_id, hit_level, taxid))
     outf.close()
     outf_sum.close()
 
