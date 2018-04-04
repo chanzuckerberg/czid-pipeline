fe639801fca8322e93eeaea3bd3ab97204fa8ab0
jshoe <github@jsheu.com>
2018-04-04 14:21:10 -0700

    Tweak

--- idseq_pipeline/commands/common.py
+++ idseq_pipeline/commands/common.py
@@ -10,10 +10,12 @@ import os
 import traceback
 import re
 
-NCBITOOL_S3_PATH = "s3://idseq-database/ncbitool" # S3 location of ncbitool executable
-ACCESSION2TAXID = 's3://czbiohub-infectious-disease/references/accession2taxid.db.gz'
-base_s3 = 's3://idseq-database/taxonomy'
+bucket = "s3://idseq-database"
+NCBITOOL_S3_PATH = bucket + "/ncbitool" # S3 location of ncbitool executable
 base_dt = '2018-02-15-utc-1518652800-unixtime__2018-02-15-utc-1518652800-unixtime'
+base_s3 = bucket + "/alignment_data"
+ACCESSION2TAXID = ("%s/%s/accession2taxid.db.gz" % (base_s3, base_dt))
+base_s3 = bucket + "/taxonomy"
 LINEAGE_SHELF = ("%s/%s/taxid-lineages.db" % (base_s3, base_dt))
 VERSION_NONE = -1
 
