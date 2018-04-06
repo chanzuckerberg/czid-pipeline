df8c32199ffa2806657a02e33a5880ee231b0b34
Yun-Fang Juan <yunfangjuan@users.noreply.github.com>
2018-04-06 23:03:59 +0000

    fix a logic error

--- idseq_pipeline/commands/non_host_alignment.py
+++ idseq_pipeline/commands/non_host_alignment.py
@@ -8,7 +8,8 @@ class Non_host_alignment(Base):
         from .non_host_alignment_functions import *
         unbuffer_stdout()
         upload_commit_sha(self.version)
-        stage1_version = os.environ.get('HOST_FILTER_PIPELINE_VERSION')
-        if stage1_version and major_version(self.version) != major_version(stage1_version):
-            raise Exception("Stage 1 and 2 run on different version: %s vs %s" % (stage1_version, self.version))
+        stage1_major_version = os.environ.get('HOST_FILTER_PIPELINE_VERSION')
+        if stage1_major_version and major_version(self.version) != stage1_major_version:
+            raise Exception("Stage 1 and 2 run on different version: %s vs %s" % (stage1_major_version, self.version))
+
         run_stage2(True)
