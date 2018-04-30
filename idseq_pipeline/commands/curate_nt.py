31d3d0696ebf4d22e82e7ca74cdb52a3e5a4070f
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-04-30 15:05:16 -0700

    wip

--- idseq_pipeline/commands/curate_nt.py
+++ idseq_pipeline/commands/curate_nt.py
@@ -1,43 +1,41 @@
 from common import *
 import shelve
 
-def remove_short(input_fasta, length_threshold, size_db_file, output_fasta):
+def remove_short(input_fasta, length_threshold, size_db_file, size_count_file, output_fasta, removed_fasta):
     '''Remove fasta records with sequence shorter than threshold; report length distribution'''
     size_count = shelve.open(size_db_file)
     size_count.clear()
-    removed = set()
     with open(input_fasta, 'rb') as input_f:
         with open(output_fasta, 'wb') as output_f:
-            line_number = 0
-            def write_output(seq_name, seq, seq_len):
-                size_count[str(seq_len)] = size_count.get(str(seq_len), 0) + 1
-                if seq_len >= length_threshold:
-                    output_f.write(seq_name + seq)
-                else:
-                    removed.add(seq_name.rstrip("\n"))
-            for line in input_f:
-                line_number += 1
-                if line_number % 100000 == 0:
-                    print "%3.1f M lines. " %  (line_number/1000000.0)
-                if line_number == 1:
-                    seq_name = line
-                    seq = ''
-                    seq_len = 0
-                elif line[0] == '>':
-                    write_output(seq_name, seq, seq_len) 
-                    seq_name = line
-                    seq = ''
-                    seq_len = 0
-                else:
-                    seq += line 
-                    seq_len += len(line.replace("\n",""))
-            write_output(seq_name, seq, seq_len)
-    for seqlen in sorted(size_count.keys(), key=lambda x: int(x)):
-        print "sequence length %s: %d records" % (seqlen, size_count[seqlen])
-    print "records removed: "
-    print removed
+            with open(removed_fasta, 'wb') as removed_f:
+                line_number = 0
+                def write_output(seq_name, seq, seq_len):
+                    size_count[str(seq_len)] = size_count.get(str(seq_len), 0) + 1
+                    if seq_len >= length_threshold:
+                        output_f.write(seq_name + seq)
+                    else:
+                        removed_f.write(seq_name + seq)
+                for line in input_f:
+                    line_number += 1
+                    if line_number % 100000 == 0:
+                        print "%3.1f M lines. " %  (line_number/1000000.0)
+                    if line_number == 1:
+                        seq_name = line
+                        seq = ''
+                        seq_len = 0
+                    elif line[0] == '>':
+                        write_output(seq_name, seq, seq_len) 
+                        seq_name = line
+                        seq = ''
+                        seq_len = 0
+                    else:
+                        seq += line 
+                        seq_len += len(line.replace("\n",""))
+                write_output(seq_name, seq, seq_len)
+    with open(size_count_file, 'wb') as count_f:
+        for seqlen in sorted(size_count.keys(), key=lambda x: int(x)):
+            count_f.write("sequence length %s: %d records" % (seqlen, size_count[seqlen]))
     size_count.close()
-    return removed
 
 def main():
     dest_dir = os.path.join(DEST_DIR, 'curate_nt')
@@ -50,7 +48,9 @@ def main():
 
     length_threshold = 100
     size_db_file = "%s/nt_seqlen.db" % dest_dir
+    size_count_file = "%s/nt_length_counts.txt" % dest_dir
     output_fasta = "%s/nt_curated.fasta" % dest_dir
+    removed_fasta = "%s/nt_removed.fasta" % dest_dir
     
     remove_short(nt_local, length_threshold, size_db_file, output_fasta)
 
