227ac5564b550e43bcebe7da17a133d71d604377
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-05-09 10:53:55 -0700

    impose limits and reduce memory usage

--- idseq_pipeline/commands/assembly_functions.py
+++ idseq_pipeline/commands/assembly_functions.py
@@ -12,6 +12,7 @@ SPADES_DIR = ROOT_DIR + '/spades' # outputs of SPAdes assemblies go here
 
 # parameters
 ASSEMBLY_READ_THRESHOLD = 100
+MAX_TAXIDS_TO_ASSEMBLE = 100
 
 # arguments from environment variables
 ALIGNMENT_S3_PATH = os.environ.get('ALIGNMENT_S3_PATH').rstrip('/')
@@ -43,19 +44,20 @@ def run_stage4():
     full_fasta = os.path.join(INPUT_DIR, TAXID_ANNOT_FASTA)
 
     # run assembly
-    def get_taxids_to_assemble(taxon_counts):
+    def get_taxids_to_assemble(taxon_counts, top=MAX_TAXIDS_TO_ASSEMBLE, read_threshold=ASSEMBLY_READ_THRESHOLD):
         total_counts = {} # sum of NT and NR counts for each taxid
         filtered_total_counts = {} # taxids that satisfy count threshold
         for item in taxon_counts:
             taxid = item['tax_id']
             total_counts[taxid] = total_counts.get(taxid, 0) + item['count']
-            if total_counts[taxid] >= ASSEMBLY_READ_THRESHOLD:
+            if read_threshold is None or total_counts[taxid] >= read_threshold:
                 filtered_total_counts[taxid] = total_counts[taxid]
         sorted_filtered_taxids = sorted(filtered_total_counts, key=filtered_total_counts.get, reverse=True)        
+        if top is not None:
+            sorted_filtered_taxids = sorted_filtered_taxids[:top]
         return sorted_filtered_taxids
 
-    def make_inputs_for_assembly():
-        # Get taxids to assemble based on criteria from report, currently: taxids with read count >= ASSEMBLY_READ_THRESHOLD
+    def make_inputs_for_assembly(lazy_run=True):
         with open(pipeline_output_json) as f:
             pipeline_output = json.load(f)
         taxon_counts = pipeline_output['pipeline_output']['taxon_counts_attributes']
@@ -64,6 +66,8 @@ def run_stage4():
         output = {}
         hit_delimiters = ['_nt', '_nr'] # annotations are like e.g. "genus_nt:543:"
         for taxid in sorted_taxids_to_assemble:
+            if lazy_run and check_s3_file_presence(os.path.join(SAMPLE_S3_OUTPUT_PATH, ASSEMBLY_DIR, taxid)):
+                continue
             pattern = '\|'.join(['%s:%s:' % (delimiter, taxid) for delimiter in hit_delimiters])
             partial_fasta =  os.path.join(RESULT_DIR, taxid + ".fasta")
             try:
@@ -72,52 +76,60 @@ def run_stage4():
             except:
                 print "WARNING: taxid %s was not found in the annotated fasta" % taxid
         # Also include the full fasta as an input to assembly
-        output['all'] = full_fasta
-        sorted_taxids_to_assemble.append('all')
+        if not lazy_run or not check_s3_file_presence(os.path.join(SAMPLE_S3_OUTPUT_PATH, ASSEMBLY_DIR, 'all')):
+            output['all'] = full_fasta
+            sorted_taxids_to_assemble.append('all')
         sorted_taxids_to_assemble = [item for item in sorted_taxids_to_assemble if item in output.keys()]
         return output, sorted_taxids_to_assemble
 
     def length_without_newlines(sequence):
         return len(sequence.replace("\n",""))
 
-    def read_fasta(input_fasta):
-        ''' Read fasta file into dictionary. Newlines remain part of the keys/values. '''
-        read_name = None
-        d = {}
-        read_order = []
-        with open(input_fasta, 'rb') as f:
-            for line in f:
-                if line.startswith('>'):
-                    read_name = line
-                    read_order += [read_name]
-                else:
-                    d[read_name] = d.get(read_name, '') + line
-        return d, read_order
-
-    def write_fasta(fasta_dict, read_order, output_file):
-        with open(output_file, 'wb') as f:
-            for read_name in read_order:
-                f.write(read_name + fasta_dict[read_name])
-
     def clean_scaffolds(spades_scaffolds_fasta, min_contig_length, cleaned_fasta):
-        fasta_dict, read_order = read_fasta(spades_scaffolds_fasta)
-        for name, sequence in fasta_dict.iteritems():
-            if length_without_newlines(sequence) <= min_contig_length:
-                read_order.remove(name)
-        write_fasta(fasta_dict, read_order, cleaned_fasta)
+        def conditional_write_record(name, sequence, output_file):
+            if length_without_newlines(sequence) > min_contig_length:
+                output_file.write(name)
+                output_file.write(sequence)
+        with open(spades_scaffolds_fasta, 'rb') as input_f:
+            with open(cleaned_fasta, 'wb') as output_f:
+                name = ''
+                sequence = ''
+                line = input_f.readline()
+                while len(line) > 0:
+                   if line.startswith('>'):
+                       conditional_write_record(name, sequence, output_f)
+                       name = line
+                       sequence = ''
+                   else:
+                      sequence += line
+                   line = input_f.readline()
+                conditional_write_record(name, sequence, output_f)
 
-    def max_read_length(input_fasta):
-        fasta_dict, _read_order = read_fasta(input_fasta)
-        return max([length_without_newlines(sequence) for sequence in fasta_dict.values()])
+    def max_read_length(input_fasta, n=2):
+        ''' get read length by looking at first n records '''
+        record_number = 0
+        read_length = 0
+        max_length = 0
+        with open(input_fasta, 'rb') as f:
+            while record_number <= n:
+                line = f.readline()
+                if line.startswith('>'):
+                    max_length = max(max_length, read_length)
+                    read_length = 0
+                    i += 1
+                else:
+                   read_length += length_without_newlines(line)
+        return max_length
 
     def spades(input_fasta, output_fasta):
         tmp_output_dir = input_fasta + "_temp_output"
         tmp_output_file = tmp_output_dir + "/scaffolds.fasta"
         try:
-            execute_command_realtime_stdout("spades.py -s %s -o %s -m 60 -t 32 --only-assembler" % (input_fasta, tmp_output_dir))
+            execute_command("spades.py -s %s -o %s -m 60 -t 32 --only-assembler" % (input_fasta, tmp_output_dir))
             subprocess.check_call("mv %s %s" % (tmp_output_file, output_fasta), shell=True)
             return True
         except:
+            traceback.print_exc()
             return False
 
     inputs, sorted_taxids = make_inputs_for_assembly()
