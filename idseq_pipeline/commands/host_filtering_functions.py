81c1804e13643130b7c86a89225fed11acca8cdf
jshoe <github@jsheu.com>
2018-06-14 16:46:14 -0700

    Try again

--- idseq_pipeline/commands/host_filtering_functions.py
+++ idseq_pipeline/commands/host_filtering_functions.py
@@ -1073,12 +1073,14 @@ def run_stage1(lazy_run=True):
     # Get list of input files to fetch
     command = "aws s3 ls %s/ | grep '\\.%s$'" % (SAMPLE_S3_INPUT_PATH,
                                                  FILE_TYPE)
-    output = execute_command_with_output(command).rstrip().split("\n")
-    if len(output) == 0 or output[0] == "":
-        # TODO: Duct-tape for now.
-        if "fastq.gz" in FILE_TYPE:
-            command = "aws s3 ls %s/ | grep '\\.%s$'" % (SAMPLE_S3_INPUT_PATH, "fq.gz")
-            output = execute_command_with_output(command).rstrip().split("\n")
+    try:
+        output = execute_command_with_output(command).rstrip().split("\n")
+    except:
+        if len(output) == 0 or output[0] == "":
+            # TODO: Duct-tape for now.
+            if "fastq.gz" in FILE_TYPE:
+                command = "aws s3 ls %s/ | grep '\\.%s$'" % (SAMPLE_S3_INPUT_PATH, "fq.gz")
+                output = execute_command_with_output(command).rstrip().split("\n")
     input_fetch_threads = []
 
     def fetch_input(input_basename):
